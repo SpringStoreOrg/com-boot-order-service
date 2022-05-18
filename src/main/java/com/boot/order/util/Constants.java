e8a61d0ddf7b0678b1478822f1b4a72d2b5df467
GeorgeT88 <59165649+GeorgeT88@users.noreply.github.com>
2022-05-18 14:19:21 +0300

    Update Constants.java

--- src/main/java/com/boot/order/util/Constants.java
+++ src/main/java/com/boot/order/util/Constants.java
@@ -2,8 +2,8 @@ package com.boot.order.util;
 
 public class Constants {
 
-	public static final String GET_CART_BY_EMAIL = "/getCartByEmail?email={email}";
-	public static final String DELETE_CART_BY_EMAIL = "/deleteCartByEmail?email={email}";
+	public static final String GET_CART_BY_EMAIL = "/?email={email}";
+	public static final String DELETE_CART_BY_EMAIL = "/?email={email}";
 
 	//Regular expression used for email validation
 	public static final String EMAIL_REGEXP = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$";
